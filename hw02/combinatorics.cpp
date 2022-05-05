6233e73a8d2f394df0aab3c2a19b11aecbc137b4
Jonas Jelten <jj@sft.lol>
2022-05-05 20:36:01 +0200

    release hw02

---
+++ hw02/combinatorics.cpp
@@ -0,0 +1,18 @@
+#include "combinatorics.h"
+
+int64_t factorial(int64_t val) {
+    // TODO
+    return 0;
+}
+
+
+int64_t permutation(int64_t val, int64_t val2) {
+    // TODO
+    return 0;
+}
+
+
+int64_t combination(int64_t val, int64_t val2) {
+    // TODO
+    return 0;
+}
