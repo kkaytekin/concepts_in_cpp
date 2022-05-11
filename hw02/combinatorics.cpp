59d741ff2787f07d54abc90f18bf556def0b61c5
Kagan <kkaytekin@gmail.com>
2022-05-11 14:48:24 +0200

    solution to exc 2

--- hw02/combinatorics.cpp
+++ hw02/combinatorics.cpp
@@ -2,17 +2,21 @@
 
 int64_t factorial(int64_t val) {
     // TODO
-    return 0;
+    // if (val < 0) return 0;
+    if (val == 0) return 1;
+    return val * factorial(val-1);
 }
 
 
 int64_t permutation(int64_t val, int64_t val2) {
     // TODO
-    return 0;
+    // if (val < val2) return 0;
+    return factorial(val)/ factorial(val-val2);
 }
 
 
 int64_t combination(int64_t val, int64_t val2) {
     // TODO
-    return 0;
+    // if (val < val2) return 0;
+    return permutation(val,val2) / factorial(val2);
 }
