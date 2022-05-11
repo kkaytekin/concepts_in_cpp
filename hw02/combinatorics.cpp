4d7f6bed03f1c4c73f90ee9f997caeec17c6259e
Kagan <kkaytekin@gmail.com>
2022-05-11 14:55:49 +0200

    Fix

--- hw02/combinatorics.cpp
+++ hw02/combinatorics.cpp
@@ -10,13 +10,13 @@ int64_t factorial(int64_t val) {
 
 int64_t permutation(int64_t val, int64_t val2) {
     // TODO
-    // if (val < val2) return 0;
-    return factorial(val)/ factorial(val-val2);
+    if (val < val2) return 0;
+    return (factorial(val) / factorial(val-val2));
 }
 
 
 int64_t combination(int64_t val, int64_t val2) {
     // TODO
-    // if (val < val2) return 0;
+    if (val < val2) return 0;
     return permutation(val,val2) / factorial(val2);
 }
